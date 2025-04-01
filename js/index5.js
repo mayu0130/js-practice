6ccc068978900edc5d0c7ef10fb13d1ea9ad3368
加藤真優子 <>
2025-04-01 14:16:47 +0900

    js/index5.js (非同期通信-await・async)

--- js/index5.js
+++ js/index5.js
@@ -80,3 +80,34 @@ const promiseTestFunc = () => {
 promiseTestFunc()
   .then(data => console.log(data))
   .catch(error => console.error("Error:",error));
+
+
+
+
+/*
+
+await・async
+
+非同期処理をより直感的に扱うための機能
+
+await
+1.非同期処理が完了するまで処理を一時停止し、その後で処理を再開するためのキーワードです。
+2.Promiseが解決されるまで、その後の処理が待機されます。
+
+async
+1.関数の前に付けてその関数が非同期であることを示します。
+2.async関数内ではawaitキーワードを使用して非同期処理の完了を待つことができます。
+
+*/
+
+//async/awaitを使った非同期処理の例
+
+async function fetchDataAsync() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  if(!response.ok){
+    console.error("Fetch error:", error);
+  }
+  const data = await response.json();
+  console.log(data);
+}
+fetchDataAsync();
