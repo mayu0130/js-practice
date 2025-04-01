cb10f3af8b425d41fd85ae267b3d4b1ccc342ced
加藤真優子 <>
2025-04-01 10:09:49 +0900

    todo.html/todo.js(TODOアプリの実装②-リファクタリング)

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,56 @@
-//todoList という配列を定義し、TODOを保存するためのリストとして使用します。l
+//todoListという配列を定義し、TODOを保存するためのリストとして使用
 let todoList = [];
+
+//入力された新しいTODOの値を取得し、todoList変数に代入する関数
+const registerNewTodo = () => {
+  //入力されたTODOの値を取得
+  const newTodoName = document.getElementById("new-todo-name");
+  const newPerson = document.getElementById("new-person");
+  const newDeadline = document.getElementById("new-deadline");
+
+  //新しいTODOをオブジェクト { todoName, person, deadline } として作成し、todoList 配列に追加。
+  todoList.push({
+    todoName: newTodoName.value,
+    person: newPerson.value,
+    deadline: newDeadline.value,
+  });
+};
+
+//TODO一覧を全て削除する関数
+const removeTodoListElem = () => {
+  const tbodyElem = document.getElementById("todo-list");
+
+  while(tbodyElem.firstChild){
+    tbodyElem.firstChild.remove();
+  };
+};
+
+//TODO一覧を表示する関数
+const appendTodoListElem = () => {
+  todoList.forEach((todo) => {
+    //todoNameのtd要素を生成
+    const todoNameTdElem = document.createElement("td");
+    todoNameTdElem.textContent = todo.todoName;
+    //personのtd要素を生成
+    const personTdElem = document.createElement("td");
+    personTdElem.textContent = todo.person;
+    //deadlineのtd要素を生成
+    const deadlineTdElem = document.createElement("td");
+    deadlineTdElem.textContent = todo.deadline;
+
+    //tr要素を作成し、td要素を挿入
+    const trElem = document.createElement("tr");
+    trElem.appendChild(todoNameTdElem);
+    trElem.appendChild(personTdElem);
+    trElem.appendChild(deadlineTdElem);
+
+
+    //tbody要素の中に、tr要素を挿入する
+    const tbodyElem = document.getElementById("todo-list");
+    tbodyElem.appendChild(trElem);
+  });
+};
+
 
 //ページが完全に読み込まれたら、以下の処理を実行します。
 document.addEventListener("DOMContentLoaded", () => {
@@ -7,47 +58,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const registerButtonElem = document.getElementById("register");
 
   registerButtonElem.addEventListener("click", () => {
-    //入力されたTODOの値を取得
-    const newTodoName = document.getElementById("new-todo-name");
-    const newPerson = document.getElementById("new-person");
-    const newDeadline = document.getElementById("new-deadline");
 
-    //新しいTODOをオブジェクト { todoName, person, deadline } として作成し、todoList 配列に追加。
-    const newTodoObject = {
-      todoName: newTodoName.value,
-      person: newPerson.value,
-      deadline: newDeadline.value,
-
-    };
-    todoList.push(newTodoObject);
-
-    //TODO一覧を全て削除
-    const tbodyElem = document.getElementById("todo-list");
-
-    while(tbodyElem.firstChild){
-      tbodyElem.firstChild.remove();
-    }
-
-    //todoListにあるすべてのTODOを、一覧に追加する処理を実行。
-    todoList.forEach((todo) => {
-      //td（表のセル）を作成し、TODOのデータを入れる。
-      const todoNameTdElem = document.createElement("td");
-      todoNameTdElem.textContent = todo.todoName;
-      const personTdElem = document.createElement("td");
-      personTdElem.textContent = todo.person;
-      const deadlineTdElem = document.createElement("td");
-      deadlineTdElem.textContent = todo.deadline;
-
-      //tr（表の行）を作成し、セルを追加。
-      const trElem = document.createElement("tr");
-      trElem.appendChild(todoNameTdElem);
-      trElem.appendChild(personTdElem);
-      trElem.appendChild(deadlineTdElem);
-
-
-      //tbody に追加して、表に新しいTODOを表示。
-      tbodyElem.appendChild(trElem);
-    });
+    registerNewTodo();
+    removeTodoListElem();
+    appendTodoListElem();
 
   });
 });
